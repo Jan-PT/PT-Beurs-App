9543e9719b7c0cb15a645bb2531eb7836397e1af
Klaas Janssens <klaas.janssens@planet-talent.com>
2015-11-30 23:25:02 +0100

    Image link index page fix (werkte niet met loop)

--- app_ci/application/views/content/start_page.php
+++ app_ci/application/views/content/start_page.php
@@ -1,3 +1,3 @@
 <div class="panel-body">
-    <div id="picture"><a href="<?php echo base_url('index.php/beursapp/info');?>"><img src="assets/images/header-home.jpg" alt="headerafbeelding" text="Planet Talent - Frames that work"/></a></div>
+    <div id="picture"><a href="<?php echo base_url('index.php/beursapp/info');?>"><img src="<?php echo base_url('assets/images/header-home.jpg');?>" alt="headerafbeelding" text="Planet Talent - Frames that work"/></a></div>
 </div>
