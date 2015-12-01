da8eb91cb08a1bdd04b362def1d3fab45b293dd7
Klaas Janssens <klaas.janssens@planet-talent.com>
2015-12-01 15:35:30 +0100

    Commentaar startpagina toegevoegd

--- app_ci/application/views/content/start_page.php
+++ app_ci/application/views/content/start_page.php
@@ -1,27 +1,9 @@
-<!-- Header die boven elke content view geladen wordt. Hierin worden de javascript files, css files en images,... geladen -->  
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<link rel="shortcut icon" href="<?php echo base_url('assets/images/icon/icon.ico');?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		<title>HTTPPTTPT</title>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/other/style.css');?>" />
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap/bootstrap.css');?>" />
-		<script src="<?php echo base_url('assets/js/jquery/jquery-2.1.4.min.js');?>"></script>
-		<script src="<?php echo base_url('assets/js/jquery/jquery-1.11.3.min.js');?>"></script>
-		<script src="<?php echo base_url('assets/js/bootstrap/bootstrap.min.js');?>"></script>
-		<script src="<?php echo base_url('assets/js/bootstrap/bootstrap.js');?>"></script>
-		<script src="<?php echo base_url('assets/js/other/random_logo.js');?>"></script>
-	</head>
-	<body>
-		<div id="container" class="container-fluid">
-                    <div class="panel-body" style="background-color: #1BA36F;">
-                        <div id="picture" >
-                            <a href="<?php echo base_url('index.php/beursapp/info');?>">
-                                <img src="<?php echo base_url('assets/images/header-home.jpg');?>" class="img-responsive" alt="headerafbeelding" text="Planet Talent - Frames that work"/>
-                            </a>
-                        </div>
-                    </div>                			
-		</div>
-	</body>
-</html>
+<!-- Startpagina view die bestaat uit een promotie afbeelding met achterliggende link -->
+<!-- waarop geklikt kan worden om naar de personal_info view te navigeren via de info functie -->
+<div class="panel-body" style="background-color: #1BA36F;">
+    <div id="picture" >
+        <a href="<?php echo base_url('index.php/beursapp/info');?>">
+            <img src="<?php echo base_url('assets/images/header-home.jpg');?>" class="img-responsive" alt="headerafbeelding" text="Planet Talent - Frames that work"/>
+        </a>
+    </div>
+</div>
